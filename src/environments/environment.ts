9eb5c6d17b6b8e1c9430ea2a5a8eb4b330f7adb8
lucas <Trannhan380381@gmail.com>
2021-11-02 11:53:26 +0700

    init firebase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,14 @@
 export const environment = {
   production: false,
   apiUrl: 'https://monigate-capstone-ubuntu.azurewebsites.net/api/',
+  firebase: {
+    apiKey: "AIzaSyA5P0-YKgjUaHKhwHk-5mci6qix4HW8PLc",
+    authDomain: "monigate-admin-webapp.firebaseapp.com",
+    projectId: "monigate-admin-webapp",
+    storageBucket: "monigate-admin-webapp.appspot.com",
+    messagingSenderId: "871094034864",
+    appId: "1:871094034864:web:192bfc75c731afbca0b675",
+  },
 };
 
 /*
