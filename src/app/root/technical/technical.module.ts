ff7829c942d8146bc8a1f54d3193523f9200b0b1
lucas <Trannhan380381@gmail.com>
2021-11-26 22:49:16 +0700

    update ui technical staff

--- src/app/root/technical/technical.module.ts
+++ src/app/root/technical/technical.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {IssueDetailComponent} from './issue-detail/issue-detail.component';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
 
 const routes: Routes = [
   {path: 'report-management', component: ReportManagementComponent},
@@ -39,6 +40,7 @@ const routes: Routes = [
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
+    MatDividerModule,
   ],
 })
 export class TechnicalModule {}
